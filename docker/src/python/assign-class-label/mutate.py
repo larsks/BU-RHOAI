a263725d458b28fdd97ef3c1d519b5b1473d8e65
Lars Kellogg-Stedman <lars@redhat.com>
2024-08-20 17:29:25 -0400

    notes from call

--- docker/src/python/assign-class-label/mutate.py
+++ docker/src/python/assign-class-label/mutate.py
@@ -6,19 +6,29 @@ from flask import Flask, request, jsonify
 from kubernetes import config, client
 from openshift.dynamic import DynamicClient
 
-webhook = Flask(__name__)
-
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-try:
-    config.load_incluster_config()
-except config.ConfigException as e:
-    LOG.error('Could not configure Kubernetes client: %s', str(e))
-    exit(1)
 
-k8s_client = client.ApiClient()
-dyn_client = DynamicClient(k8s_client)
+def get_client():
+    try:
+        config.load_config()
+    except config.ConfigException as e:
+        LOG.error('Could not configure Kubernetes client: %s', str(e))
+        exit(1)
+
+    k8s_client = client.ApiClient()
+    dyn_client = DynamicClient(k8s_client)
+
+    return dyn_client
+
+def get_group_members(group_name):
+    dyn_client = get_client()
+    group_resource = dyn_client.resources.get(
+        api_version='user.openshift.io/v1', kind='Group'
+    )
+    group_obj = group_resource.get(name=group)
+    return group_obj.users
 
 
 def assign_class_label(pod, groups):
@@ -29,51 +39,74 @@ def assign_class_label(pod, groups):
 
     # Iterate through classes
     for group in groups:
-        try:
-            # Get users in group (class)
-            group_resource = dyn_client.resources.get(
-                api_version='user.openshift.io/v1', kind='Group'
+        users = get_group_members(group)
+
+        # Get users in group (class)
+
+        # Check if group has no users
+        if not users:
+            LOG.warning(
+                f'Group {group} has no users or users attribute is not a list.'
             )
-            group_obj = group_resource.get(name=group)
-            group_obj_dict = group_obj.to_dict()
-            group_users = group_obj_dict.get('users', [])
-
-            # Check if group has no users
-            if not group_users:
-                LOG.warning(
-                    f'Group {group} has no users or users attribute is not a list.'
-                )
-                continue
-
-            # Compare users in the groups (classes) with the pod user
-            if pod_user in group_users:
-                LOG.info(f'Assigning class label: {group} to user {pod_user}')
-                return group
-        except Exception as e:
-            LOG.error(f'Error fetching group {group}: {str(e)}')
             continue
+
+        # Compare users in the groups (classes) with the pod user
+        if pod_user in users:
+            LOG.info(f'Assigning class label: {group} to user {pod_user}')
+            return group
 
     return None
 
 
-@webhook.route('/mutate', methods=['POST'])
-def mutate_pod():
-    # Grab pod for mutation
-    request_info = request.get_json()
-    uid = request_info['request']['uid']
-    pod = request_info['request']['object']
 
-    groups = os.environ.get('GROUPS').split(',')
+def create_app(**config):
+    app = Flask(__name__)
+    app.config.from_prefixed_env('RHOAI')
+    app.config.update(config)
 
-    if not groups:
+
+    if not app.config['GROUPS']:
         LOG.error('GROUPS environment variables are required.')
         exit(1)
 
-    # Grab class that the pod user belongs to
-    class_label = assign_class_label(pod, groups)
+    groups = app.config['GROUPS'].split(',')
 
-    # If user not in any class, return without modifications
-    if not class_label:
+    @app.route('/mutate', methods=['POST'])
+    def mutate_pod():
+        # Grab pod for mutation
+        request_info = request.get_json()
+        uid = request_info['request']['uid']
+        pod = request_info['request']['object']
+
+        # Grab class that the pod user belongs to
+        try:
+            class_label = assign_class_label(pod, groups)
+        except Exception as err:
+            LOG.error("failed to assign class label: %s", err)
+            return "unexpected error encountered", 500, {'content-type':'text/plain'}
+
+        # If user not in any class, return without modifications
+        if not class_label:
+            return jsonify(
+                {
+                    'apiVersion': 'admission.k8s.io/v1',
+                    'kind': 'AdmissionReview',
+                    'response': {
+                        'uid': uid,
+                        'allowed': True,
+                        'status': {'message': 'No class label assigned.'},
+                    },
+                }
+            )
+
+        # Generate JSON Patch to add class label
+        patch = [{'op': 'add', 'path': '/metadata/labels/class', 'value': class_label}]
+
+        # Encode patch as base64 for response
+        patch_base64 = base64.b64encode(json.dumps(patch).encode('utf-8')).decode(
+            'utf-8'
+        )
+        # Return webhook response that includes the patch to add class label
         return jsonify(
             {
                 'apiVersion': 'admission.k8s.io/v1',
@@ -81,32 +114,10 @@ def mutate_pod():
                 'response': {
                     'uid': uid,
                     'allowed': True,
-                    'status': {'message': 'No class label assigned.'},
+                    'patchType': 'JSONPatch',
+                    'patch': patch_base64,
                 },
             }
         )
 
-    # Generate JSON Patch to add class label
-    patch = [{'op': 'add', 'path': '/metadata/labels/class', 'value': class_label}]
-
-    # Encode patch as base64 for response
-    patch_base64 = base64.b64encode(json.dumps(patch).encode('utf-8')).decode('utf-8')
-
-    # Return webhook response that includes the patch to add class label
-    return jsonify(
-        {
-            'apiVersion': 'admission.k8s.io/v1',
-            'kind': 'AdmissionReview',
-            'response': {
-                'uid': uid,
-                'allowed': True,
-                'patchType': 'JSONPatch',
-                'patch': patch_base64,
-            },
-        }
-    )
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level='INFO')
-    webhook.run(host='0.0.0.0', port=8443)
+    return app
