ef6e1ca31c3c81dc76ab55b0cb86c1866d3ba9db
Isaiah Stapleton <istaplet@redhat.com>
2024-10-22 11:25:45 -0400

    ope-log-collector: clean up code
    Signed-off-by: Isaiah Stapleton <istaplet@redhat.com>

--- container-images/ope-log-collector/collect-logs.py
+++ container-images/ope-log-collector/collect-logs.py
@@ -4,10 +4,19 @@ from kubernetes_asyncio import client, config, watch
 import aiofiles
 import asyncio
 import traceback
+from pydantic import BaseModel
 
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level='INFO', format='%(message)s')
+
+
+class LogInfo(BaseModel, arbitrary_types_allowed=True):
+    v1: client.CoreV1Api
+    namespace: str
+    pod_name: str
+    container_name: str
+    pvc_name: str
 
 
 async def get_client() -> client.CoreV1Api:
@@ -22,15 +31,27 @@ async def get_client() -> client.CoreV1Api:
 async def gather_logs(namespace: str):
     v1 = await get_client()
 
+    LOG_DIR = './log'
+
+    if not os.path.exists(LOG_DIR):
+        try:
+            os.makedirs(LOG_DIR)
+        except Exception as e:
+            LOG.error(f"Could not create log directory '{LOG_DIR}': {e}")
+            exit(1)
+
+    # # For handling server side timeout issue
+    # first_run = True
+    # if not first_run:
+    #     since_seconds = 5
+    # else:
+    #     since_seconds = 99999999999999
+
     while True:
         w = watch.Watch()
-        log_tasks = {}
-        event_tasks = {}
-        pvc_tasks = {}
+        tasks = {}
         try:
-            async for event in w.stream(
-                v1.list_namespaced_pod, namespace=namespace, timeout_seconds=0
-            ):
+            async for event in w.stream(v1.list_namespaced_pod, namespace=namespace):
                 pod = event['object']
                 pod_name = pod.metadata.name
                 event_type = event['type']
@@ -49,80 +70,62 @@ async def gather_logs(namespace: str):
 
                     container_name = pod.spec.containers[0].name
 
+                    log_info = LogInfo(
+                        v1=v1,
+                        namespace=namespace,
+                        pod_name=pod_name,
+                        container_name=container_name,
+                        pvc_name=pvc_name,
+                    )
+
                     # Start streaming logs
-                    if pod_name not in log_tasks:
-                        log_tasks[pod_name] = asyncio.create_task(
-                            stream_pod_logs(v1, namespace, pod_name, container_name)
+                    if pod_name not in tasks:
+                        tasks[pod_name] = asyncio.gather(
+                            stream_pod_logs(log_info),
+                            stream_pod_events(log_info),
+                            stream_pvc_events(log_info),
                         )
-                        event_tasks[pod_name] = asyncio.create_task(
-                            stream_pod_events(v1, namespace, pod_name)
-                        )
-                        pvc_tasks[pod_name] = asyncio.create_task(
-                            stream_pvc_events(v1, namespace, pod_name, pvc_name)
-                        )
+
                 elif event_type == 'DELETED':
                     LOG.info(f'Pod deleted: {pod_name}. Cancelling log streaming.')
-                    if pod_name in log_tasks:
-                        log_tasks[pod_name].cancel()
-                        del log_tasks[pod_name]
-                    if pod_name in event_tasks:
-                        event_tasks[pod_name].cancel()
-                        del event_tasks[pod_name]
-                    if pod_name in pvc_tasks:
-                        pvc_tasks[pod_name].cancel()
-                        del pvc_tasks[pod_name]
+                    if pod_name in tasks:
+                        tasks[pod_name].cancel()
+                        del tasks[pod_name]
+
+        except asyncio.CancelledError:
+            LOG.info('User cancelled run. Cancelling log streaming.')
+            exit(1)
 
         except Exception as e:
-            LOG.info(f'Server side Timeout: {e}. Re-establishing connection.')
+            LOG.info(f'Unexpected Error: {e}. Re-establishing connection.')
 
             await w.close()
 
-            for task in log_tasks.values():
+            for task in tasks.values():
                 task.cancel()
 
-            for event in event_tasks.values():
-                event.cancel()
-
-            for event in pvc_tasks.values():
-                event.cancel()
-
-            await asyncio.gather(*log_tasks.values(), return_exceptions=True)
-
-            await asyncio.gather(*event_tasks.values(), return_exceptions=True)
-
-            await asyncio.gather(*pvc_tasks.values(), return_exceptions=True)
+            await asyncio.gather(*tasks.values(), return_exceptions=True)
 
             await asyncio.sleep(5)
 
 
-async def stream_pod_logs(
-    v1: client.CoreV1Api, namespace: str, pod_name: str, container_name: str
-):
-    LOG_DIR = './log'
-
-    if not os.path.exists(LOG_DIR):
-        try:
-            os.makedirs(LOG_DIR)
-        except Exception as e:
-            LOG.error(f"Could not create log directory '{LOG_DIR}': {e}")
-            exit(1)
-
-    log_file_path = os.path.join(LOG_DIR, f'{pod_name}.log')
-    LOG.info(f"Streaming logs for pod '{pod_name}' to '{log_file_path}'.")
+async def stream_pod_logs(log_info: LogInfo):
+    log_file_path = os.path.join('./log', f'{log_info.pod_name}.log')
+    LOG.info(f"Streaming logs for pod '{log_info.pod_name}' to '{log_file_path}'.")
 
     response = None
     try:
-        response = await v1.read_namespaced_pod_log(
-            name=pod_name,
-            namespace=namespace,
-            container=container_name,
+        response = await log_info.v1.read_namespaced_pod_log(
+            name=log_info.pod_name,
+            namespace=log_info.namespace,
+            container=log_info.container_name,
             follow=True,
             _preload_content=False,
             timestamps=True,
         )
 
         # Read logs and write to log file
-        async with aiofiles.open(log_file_path, 'w') as log_file:
+        async with aiofiles.open(log_file_path, 'a') as log_file:
             async for line in response.content:
                 if line:
                     decoded_line = line.decode('utf-8')
@@ -132,7 +135,9 @@ async def stream_pod_logs(
         if response:
             await response.release()
     except Exception as e:
-        LOG.error(f"Unexpected error while streaming logs for pod '{pod_name}': {e}")
+        LOG.error(
+            f"Unexpected error while streaming logs for pod '{log_info.pod_name}': {e}"
+        )
         LOG.error(traceback.format_exc())
         raise
     finally:
@@ -142,79 +147,22 @@ async def stream_pod_logs(
     await asyncio.sleep(5)
 
 
-async def stream_pod_events(v1: client.CoreV1Api, namespace: str, pod_name: str):
-    LOG_DIR = './log'
+async def stream_pod_events(log_info: LogInfo):
+    log_file_path = os.path.join('./log', f'{log_info.pod_name}-pod-events.log')
+    LOG.info(
+        f"Streaming pod events for pod '{log_info.pod_name}' to '{log_file_path}'."
+    )
 
-    if not os.path.exists(LOG_DIR):
-        try:
-            os.makedirs(LOG_DIR)
-        except Exception as e:
-            LOG.error(f"Could not create log directory '{LOG_DIR}': {e}")
-            exit(1)
-
-    log_file_path = os.path.join(LOG_DIR, f'{pod_name}-pod-events.log')
-    LOG.info(f"Streaming pod events for pod '{pod_name}' to '{log_file_path}'.")
-
-    field_selector = f'involvedObject.kind=Pod,involvedObject.name={pod_name},involvedObject.namespace={namespace}'
+    field_selector = f'involvedObject.kind=Pod,involvedObject.name={log_info.pod_name},involvedObject.namespace={log_info.namespace}'
 
     response = None
     try:
-        events = await v1.list_namespaced_event(
-            namespace=namespace, field_selector=field_selector
+        events = await log_info.v1.list_namespaced_event(
+            namespace=log_info.namespace, field_selector=field_selector
         )
 
         # Read logs and write to log file
-        async with aiofiles.open(log_file_path, 'w') as log_file:
-            for event in events.items:
-                if event:
-                    event_time = event.last_timestamp
-                    event_type = event.type
-                    event_message = event.message
-
-                    event_entry = f'{event_time} | Type: {event_type} | Message: {event_message}\n'
-
-                    await log_file.write(event_entry)
-                    await log_file.flush()
-
-    except asyncio.CancelledError:
-        if response:
-            await response.release()
-    except Exception as e:
-        LOG.error(f"Unexpected error while streaming events for pod '{pod_name}': {e}")
-        LOG.error(traceback.format_exc())
-        raise
-    finally:
-        if response:
-            await response.release()
-
-    await asyncio.sleep(5)
-
-
-async def stream_pvc_events(
-    v1: client.CoreV1Api, namespace: str, pod_name: str, pvc_name: str
-):
-    LOG_DIR = './log'
-
-    if not os.path.exists(LOG_DIR):
-        try:
-            os.makedirs(LOG_DIR)
-        except Exception as e:
-            LOG.error(f"Could not create log directory '{LOG_DIR}': {e}")
-            exit(1)
-
-    log_file_path = os.path.join(LOG_DIR, f'{pod_name}-pvc-events.log')
-    LOG.info(f"Streaming pvc events for pod '{pod_name}' to '{log_file_path}'.")
-
-    field_selector = f'involvedObject.kind=PersistentVolumeClaim,involvedObject.name={pvc_name},involvedObject.namespace={namespace}'
-
-    response = None
-    try:
-        events = await v1.list_namespaced_event(
-            namespace=namespace, field_selector=field_selector
-        )
-
-        # Read logs and write to log file
-        async with aiofiles.open(log_file_path, 'w') as log_file:
+        async with aiofiles.open(log_file_path, 'a') as log_file:
             for event in events.items:
                 if event:
                     event_time = event.last_timestamp
@@ -231,7 +179,50 @@ async def stream_pvc_events(
             await response.release()
     except Exception as e:
         LOG.error(
-            f"Unexpected error while streaming pvc events for pod '{pod_name}': {e}"
+            f"Unexpected error while streaming events for pod '{log_info.pod_name}': {e}"
+        )
+        LOG.error(traceback.format_exc())
+        raise
+    finally:
+        if response:
+            await response.release()
+
+    await asyncio.sleep(5)
+
+
+async def stream_pvc_events(log_info: LogInfo):
+    log_file_path = os.path.join('./log', f'{log_info.pod_name}-pvc-events.log')
+    LOG.info(
+        f"Streaming pvc events for pod '{log_info.pod_name}' to '{log_file_path}'."
+    )
+
+    field_selector = f'involvedObject.kind=PersistentVolumeClaim,involvedObject.name={log_info.pvc_name},involvedObject.namespace={log_info.namespace}'
+
+    response = None
+    try:
+        events = await log_info.v1.list_namespaced_event(
+            namespace=log_info.namespace, field_selector=field_selector
+        )
+
+        # Read logs and write to log file
+        async with aiofiles.open(log_file_path, 'a') as log_file:
+            for event in events.items:
+                if event:
+                    event_time = event.last_timestamp
+                    event_type = event.type
+                    event_message = event.message
+
+                    event_entry = f'{event_time} | Type: {event_type} | Message: {event_message}\n'
+
+                    await log_file.write(event_entry)
+                    await log_file.flush()
+
+    except asyncio.CancelledError:
+        if response:
+            await response.release()
+    except Exception as e:
+        LOG.error(
+            f"Unexpected error while streaming pvc events for pod '{log_info.pod_name}': {e}"
         )
         LOG.error(traceback.format_exc())
         raise
